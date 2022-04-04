98002a1f3f9d118702e93fc91bb8c3e1ecbd4b2f
anthonyjlau <ajlau@hawaii.edu>
2022-04-03 22:10:33 -1000

    notes-1 finished in 41 minutes

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -5,6 +5,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Contact from '../components/Contact';
 import { Contacts } from '../../api/contact/Contacts';
+import { Notes } from '../../api/note/Notes';
 
 /** Renders a table containing all of the Contact documents. Usee ContactItem> to render each row. */
 class ListContacts extends React.Component {
@@ -20,7 +21,7 @@ class ListContacts extends React.Component {
       <Container>
         <Header as="h2" textAlign="center" inverted>List Contacts</Header>
         <Card.Group>
-          {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+          {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact} notes={this.props.notes.filter(note => (note.contactId === contact._id))}/>)}
         </Card.Group>
       </Container>
     );
@@ -30,6 +31,7 @@ class ListContacts extends React.Component {
 // Require an array oe Contact documents in the props.
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
+  notes: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -37,12 +39,15 @@ ListContacts.propTypes = {
 export default withTracker(() => {
   // Get access te Contact documents.
   const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscription2 = Meteor.subscribe(Notes.userPublicationName);
   // Determine if the subscription is ready
-  const ready = subscription.ready();
+  const ready = subscription.ready() && subscription2.ready();
   // Get the Contact documents
   const contacts = Contacts.collection.find({}).fetch();
+  const notes = Notes.collection.find({}).fetch();
   return {
     contacts,
+    notes,
     ready,
   };
 })(ListContacts);
